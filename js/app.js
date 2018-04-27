a1b868020b049ef3ddc5be1410f1c9e0aef27631
Nelson <nelsondcraig@gmail.com>
2018-04-26 18:20:43 -0700

    paddle moving now!

--- js/app.js
+++ js/app.js
@@ -4,8 +4,31 @@ var ctx = canvas.getContext('2d');
 canvas.height = document.getElementById('container').clientHeight;
 canvas.width = document.getElementById('container').clientWidth;
 
-ctx.width = canvas.width - 20;
-ctx.height = canvas.height - 20;
+ctx.width = canvas.width;
+ctx.height = canvas.height;
 
+function animate() {
+  window.requestAnimationFrame(step)
+}
 
-window.document.onload = new Game();
+function step(key) {
+  ctx.clearRect(0, 0, canvas.height, canvas.width);
+  board.draw();
+  playOne.move(key);
+  playTwo.draw();
+  ball.draw();
+}
+
+window.addEventListener('keydown', function(e) {
+  console.log(e.keyCode);
+  if(e.keyCode === 38) {
+    step(38);
+  } else if(e.keyCode === 40) {
+    step(40);
+  }
+});
+
+const board = new Board;
+const playOne = new PlayerOne;
+const playTwo = new PlayerTwo;
+const ball = new Ball;
